f3a6ef82f607da841ffda1acbc41517a10f99509
Enrique Orozco Jr. <enrique-orozco@outlook.com>
2025-08-30 15:42:00 -0400

    progress

--- uart_if.sv
+++ uart_if.sv
@@ -1,25 +1,43 @@
 ///////////////////////////////////////////////////////////////////////////////
 // PROJECT: UART Communication System with Error Detection
 ///////////////////////////////////////////////////////////////////////////////
+// FILENAME: uart_if.sv
 // AUTHORS: Enrique Orozco Jr. <enrique-orozco@outlook.com>
+// DESCRIPTION: This file describes the encapsulation of all the wires 
+// that will be utilized in the UART communication system.
 ///////////////////////////////////////////////////////////////////////////////
 
-interface uart_if;
+interface uart_if
+  #(parameter DATA_WIDTH = 8);
 
-  // Shared Inputs:
-  logic clk;
+  // Shared Input:
   logic reset;
 
   // Transmitter Inputs:
+  logic [DATA_WIDTH-1:0] TxData;
   logic transmit;
-  logic [7:0] TxData;
 
   // Transmitter Outputs:
   logic TxD;
   logic busy;
 
   // Receiver Outputs:
-  logic [7:0] RxData;
+  logic [DATA_WIDTH-1:0] RxData;
   logic valid_rx;
+
+  modport tx (
+    input TxData,
+    input transmit,
+    input reset,
+    output TxD,
+    output busy
+  );
+
+  modport rx (
+    input TxD,
+    input reset,
+    output RxData,
+    output valid_rx
+  );
 
 endinterface
--- uart_tx.sv
+++ uart_tx.sv
@@ -4,5 +4,15 @@
 // AUTHORS: Enrique Orozco Jr. <enrique-orozco@outlook.com>
 ///////////////////////////////////////////////////////////////////////////////
 
-module Uart_Receiver(uart_if uart);
+module uart_tx(uart_if uart);
+
+  parameter int CLK_FREQ = 100_000_000;
+  parameter int BAUD_RATE = 115_200;
+
+  localparam int CYCLES_PER_BIT = CLK_FREQ / BAUD_RATE;
+  localparam int COUNTER_WIDTH = $clog2(CYCLES_PER_BIT);
+
+  logic [COUNTER_WIDTH-1:0] cycle_counter;
+ 
+
 endmodule
